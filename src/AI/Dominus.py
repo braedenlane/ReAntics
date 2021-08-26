e119e32c0fe10b2d907edde00a6310689ea8d6ea
braedenlane <braedenlane10@gmail.com>
2021-08-26 11:59:12 -0700

    Dominus now is a copy of Random, scaffolding for changes later
    - Added getPlacement and getMove methods, full "functioning" AI now

--- src/AI/Dominus.py
+++ src/AI/Dominus.py
@@ -37,6 +37,80 @@ class AIPlayer(Player):
         self.myTunnel = None
 
     ##
+    # getPlacement
+    #
+    # Description: called during setup phase for each Construction that
+    #   must be placed by the player.  These items are: 1 Anthill on
+    #   the player's side; 1 tunnel on player's side; 9 grass on the
+    #   player's side; and 2 food on the enemy's side.
+    #
+    # Parameters:
+    #   construction - the Construction to be placed.
+    #   currentState - the state of the game at this point in time.
+    #
+    # Return: The coordinates of where the construction is to be placed
+    ##
+    def getPlacement(self, currentState):
+        numToPlace = 0
+        # implemented by students to return their next move
+        if currentState.phase == SETUP_PHASE_1:  # stuff on my side
+            numToPlace = 11
+            moves = []
+            for i in range(0, numToPlace):
+                move = None
+                while move == None:
+                    # Choose any x location
+                    x = random.randint(0, 9)
+                    # Choose any y location on your side of the board
+                    y = random.randint(0, 3)
+                    # Set the move if this space is empty
+                    if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        move = (x, y)
+                        # Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        currentState.board[x][y].constr == True
+                moves.append(move)
+            return moves
+        elif currentState.phase == SETUP_PHASE_2:  # stuff on foe's side
+            numToPlace = 2
+            moves = []
+            for i in range(0, numToPlace):
+                move = None
+                while move == None:
+                    # Choose any x location
+                    x = random.randint(0, 9)
+                    # Choose any y location on enemy side of the board
+                    y = random.randint(6, 9)
+                    # Set the move if this space is empty
+                    if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        move = (x, y)
+                        # Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        currentState.board[x][y].constr == True
+                moves.append(move)
+            return moves
+        else:
+            return [(0, 0)]
+
+    ##
+    # getMove
+    # Description: Gets the next move from the Player.
+    #
+    # Parameters:
+    #   currentState - The state of the current game waiting for the player's move (GameState)
+    #
+    # Return: The Move to be made
+    ##
+    def getMove(self, currentState):
+        moves = listAllLegalMoves(currentState)
+        selectedMove = moves[random.randint(0, len(moves) - 1)];
+
+        # don't do a build move if there are already 3+ ants
+        numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
+        while (selectedMove.moveType == BUILD and numAnts >= 3):
+            selectedMove = moves[random.randint(0, len(moves) - 1)];
+
+        return selectedMove
+
+    ##
     # getAttack
     # Description: Gets the attack to be made from the Player
     #
