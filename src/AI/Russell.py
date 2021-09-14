be4894474bc5d29016874dca2d3573088b731d76
braedenlane <braedenlane10@gmail.com>
2021-09-13 22:03:36 -0700

    Russell is functioning. Completed Assignment 2 for CS421

--- src/AI/Russell.py
+++ src/AI/Russell.py
@@ -8,7 +8,9 @@ from Ant import UNIT_STATS
 from Move import Move
 from GameState import *
 from AIPlayerUtils import *
+
 sys.path.append("..")  # so other modules can be found in parent dir
+
 
 ##
 #
@@ -111,12 +113,11 @@ class AIPlayer(Player):
         for move in moves:
             toAdd = self.getNode(moves[count], states[count], 1, rootNode)
             nodes.append(toAdd)
-            count+=1
+            count += 1
 
         selectedMove = self.bestMove(nodes)
 
         return selectedMove
-
 
     ##
     # utility
@@ -140,34 +141,34 @@ class AIPlayer(Player):
         rating += 0.07 * myInv.foodCount
 
         # add .075 for each worker we have, up to 2 (favor worker over just keeping a food)
-        if(len(getAntList(state, state.whoseTurn, (WORKER,))) <= 2):
+        if (len(getAntList(state, state.whoseTurn, (WORKER,))) <= 2):
             rating += 0.075 * len(getAntList(state, state.whoseTurn, (WORKER,)))
 
         # add .2 if we have a drone
-        if(len(getAntList(state, state.whoseTurn, (DRONE,))) == 1):
+        if (len(getAntList(state, state.whoseTurn, (DRONE,))) == 1):
             rating += 0.2
 
-        #add .25 if we have a soldier
-        if(len(getAntList(state, state.whoseTurn, (SOLDIER,))) == 1):
+        # add .25 if we have a soldier
+        if (len(getAntList(state, state.whoseTurn, (SOLDIER,))) == 1):
             rating += 0.25
 
         # Evaluate how close this state is to adding a piece of food
         for ant in myInv.ants:
 
             # Queen behavior (keep off anthill)
-            if(ant.type == QUEEN):
-                if(myInv.getAnthill().coords == ant.coords):
+            if (ant.type == QUEEN):
+                if (myInv.getAnthill().coords == ant.coords):
                     return 0.0
 
             # Worker behavior
-            if(ant.type == WORKER):
+            if (ant.type == WORKER):
                 stepsTilFood = 0
                 stepsTilConstr = 0
 
                 # calculate the closest food source on my side
                 if not (ant.carrying):
-                    stepsTilFood = 100 # ie infinity
-                    stepsTilConstr = 100 # ie infinity
+                    stepsTilFood = 100  # ie infinity
+                    stepsTilConstr = 100  # ie infinity
                     foods = getCurrPlayerFood(self, state)
                     for food in foods:
                         potentialSteps = stepsToReach(state, ant.coords, food.coords)
@@ -203,22 +204,22 @@ class AIPlayer(Player):
 
             # Drone behavior
 
-            if(ant.type == DRONE):
+            if (ant.type == DRONE):
                 shortestPathToEnemWorker = 0
                 if not (len(getAntList(state, 1 - state.whoseTurn, (WORKER,))) == 0):
-                    shortestPathToEnemWorker = 100 # ie infinity
+                    shortestPathToEnemWorker = 100  # ie infinity
                     for enemAnt in enemInv.ants:
-                        if(enemAnt.type == WORKER):
+                        if (enemAnt.type == WORKER):
                             potentialSteps = stepsToReach(state, ant.coords, enemAnt.coords)
-                            if(potentialSteps < shortestPathToEnemWorker):
+                            if (potentialSteps < shortestPathToEnemWorker):
                                 shortestPathToEnemWorker = potentialSteps
 
                 rating -= 0.005 * shortestPathToEnemWorker
 
             # Soldier behavior
 
-            if(ant.type == SOLDIER):
-                shortestPathToEnemQueen = 100 # ie infinity
+            if (ant.type == SOLDIER):
+                shortestPathToEnemQueen = 100  # ie infinity
                 for enemAnt in enemInv.ants:
                     if (enemAnt.type == QUEEN):
                         potentialSteps = stepsToReach(state, ant.coords, enemAnt.coords)
@@ -276,8 +277,6 @@ class AIPlayer(Player):
         bestMove = bestNode["move"]
         return bestMove
 
-
-
     ##
     # getAttack
     # Description: Gets the attack to be made from the Player
@@ -299,3 +298,73 @@ class AIPlayer(Player):
     def registerWin(self, hasWon):
         # method templaste, not implemented
         pass
+
+
+#############################
+##   Unit test functions   ##
+#############################
+
+p = AIPlayer(1)
+
+
+##
+# testUtility
+#
+# tests if the the utility function evaluates the board correctly
+##
+def testUtility():
+    gamestate = GameState.getBasicState()
+    score = p.utility(gamestate)
+    assert score <= 1.0
+    assert score >= 0.0
+
+
+##
+# testGetNode
+#
+# tests if the get node function returns the correct node
+##
+def testGetNode():
+    move = Move(0)
+    gamestate = GameState.getBasicState()
+    depth = 2
+    eval = p.utility(gamestate)
+    parentNode = None
+    node = p.getNode(move, gamestate, depth, parentNode)
+
+    assert move == node["move"]
+    assert gamestate == node["state"]
+    assert depth == node["depth"]
+    assert eval == node["evaluation"]
+    assert parentNode == node["parentNode"]
+
+
+##
+# testBestMove
+#
+# tests if the best move function returns the best move properly
+##
+def testBestMove():
+    move1 = Move(0)
+    move2 = Move(1)
+    gamestate = GameState.getBasicState()
+    depth = 2
+    parent = None
+
+    node1 = p.getNode(move1, gamestate, depth, parent)
+    node1["evaluation"] = .9
+
+    node2 = p.getNode(move2, gamestate, depth, parent)
+    node2["evaluation"] = .6
+
+    assert move1 == p.bestMove([node1, node2])
+
+
+########################
+## Calling unit tests ##
+########################
+
+
+testUtility()
+testGetNode()
+testBestMove()
